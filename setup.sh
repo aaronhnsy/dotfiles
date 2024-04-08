166449b87bb0516012ba4c7ab7af46f1c263d46f
aaronhnsy <aaronhnsy@gmail.com>
2024-04-08 04:27:43 +0100

    reorganise packages into shell and tools folders

--- setup.sh
+++ setup.sh
@@ -9,31 +9,31 @@ mkdir -p "$HOME/.local/state"
 
 stow_package() {
     NAME=$1; DIR=$2
-    if [ ! -d "$DIR" ]; then
-        echo "Stowing '$NAME'."
+    if [ -d "$DIR" ]; then
+        echo "Package '$NAME' is already stowed."
+    else
         mkdir -p "$DIR"
         stow "$NAME"
-    else
-        echo "Package '$NAME' already stowed."
+        echo "Created '$DIR' and stowed '$NAME'."
     fi
 }
 stow_package ssh "$HOME/.ssh/"
 stow_package zsh "$HOME/.config/zsh/"
-stow_package starship "$HOME/.config/starship/"
 stow_package wget "$HOME/.config/wget/"
 stow_package tmux "$HOME/.config/tmux/"
-stow_package neofetch "$HOME/.config/neofetch/"
 stow_package htop "$HOME/.config/htop/"
+stow_package neofetch "$HOME/.config/neofetch/"
+stow_package starship "$HOME/.config/starship/"
 
 install_tool() {
     NAME=$1; URL=$2; DIR=$3
-    if [ ! -d "$DIR" ]; then
+    if [ -d "$DIR" ]; then
+        echo "Tool '$NAME' is already installed."
+    else
         echo "Installing '$NAME':"
         git clone "$URL" "$DIR"
         cd "$DIR"
         git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-    else
-        echo "Tool '$NAME' is already installed."
     fi
 }
 install_tool "pyenv" "https://github.com/pyenv/pyenv" "$HOME/.local/share/pyenv"
