c8e90fbd250b416f5a891c9c771cfbe542081c31
aaronhnsy <aaronhnsy@gmail.com>
2025-08-01 12:30:16 +0100

    only do brew stuff on macos

--- main/.config/fish/conf.d/03_brew.fish
+++ main/.config/fish/conf.d/03_brew.fish
@@ -1,5 +1,7 @@
-# disable homebrew hints
-set -gx HOMEBREW_NO_ENV_HINTS "true"
+if test (uname) = Darwin
+    # disable homebrew hints
+    set -gx HOMEBREW_NO_ENV_HINTS "true"
 
-# set homebrew environment variables
-eval "$(/opt/homebrew/bin/brew shellenv fish)"
+    # set homebrew environment variables
+    eval "$(/opt/homebrew/bin/brew shellenv fish)"
+end
