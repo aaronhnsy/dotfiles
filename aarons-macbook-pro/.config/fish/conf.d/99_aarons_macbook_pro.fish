17e7a0a39eae3385fb352a1b56efa3bde980d737
aaronhnsy <aaronhnsy@gmail.com>
2025-09-08 23:50:32 +0100

    add jetbrains scripts dir to path on macos

--- aarons-macbook-pro/.config/fish/conf.d/99_aarons_macbook_pro.fish
+++ aarons-macbook-pro/.config/fish/conf.d/99_aarons_macbook_pro.fish
@@ -1,2 +1,5 @@
 # set SSH_AUTH_SOCK to the 1Password agent socket
 set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+
+# add jetbrains toolbox scripts to path
+fish_add_path -g "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
