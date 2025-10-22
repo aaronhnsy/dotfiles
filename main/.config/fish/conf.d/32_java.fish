e6ed6c2e44703170cb44eb91f0aa0e29f590b5cf
aaronhnsy <aaronhnsy@gmail.com>
2025-10-22 07:01:34 +0100

    add maven java xdg dirs

--- main/.config/fish/conf.d/32_java.fish
+++ main/.config/fish/conf.d/32_java.fish
@@ -1,2 +1,8 @@
 # set gradle data directory
 set -gx GRADLE_USER_HOME "$XDG_DATA_HOME/gradle"
+
+# set maven data directory
+set -gx MAVEN_OPTS "-Dmaven.repo.local=$XDG_DATA_HOME/maven/repository"
+
+# set maven config directory
+set -gx MAVEN_ARGS "--settings $XDG_CONFIG_HOME/maven/settings.xml"
