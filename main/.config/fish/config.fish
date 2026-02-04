96f274d7793f97d553c13d8eb2386bb5aed12f84
aaronhnsy <aaronhnsy@gmail.com>
2026-02-04 15:14:11 +0000

    set fish theme

--- main/.config/fish/config.fish
+++ main/.config/fish/config.fish
@@ -2,44 +2,7 @@
 set -g fish_greeting
 
 # theme
-set --global fish_color_autosuggestion 707A8C
-set --global fish_color_cancel --reverse
-set --global fish_color_command 5CCFE6
-set --global fish_color_comment 5C6773
-set --global fish_color_cwd 73D0FF
-set --global fish_color_cwd_root red
-set --global fish_color_end F29E74
-set --global fish_color_error FF3333
-set --global fish_color_escape 95E6CB
-set --global fish_color_history_current --bold
-set --global fish_color_host normal
-set --global fish_color_host_remote yellow
-set --global fish_color_keyword 5CCFE6
-set --global fish_color_match F28779
-set --global fish_color_normal CBCCC6
-set --global fish_color_operator FFCC66
-set --global fish_color_option CBCCC6
-set --global fish_color_param CBCCC6
-set --global fish_color_quote BAE67E
-set --global fish_color_redirection D4BFFF
-set --global fish_color_search_match --bold --background=FFCC66
-set --global fish_color_selection --bold --background=FFCC66
-set --global fish_color_status red
-set --global fish_color_user brgreen
-set --global fish_color_valid_path --underline=single
-set --global fish_pager_color_background
-set --global fish_pager_color_completion normal
-set --global fish_pager_color_description B3A06D
-set --global fish_pager_color_prefix normal --bold --underline=single
-set --global fish_pager_color_progress brwhite --bold --background=cyan
-set --global fish_pager_color_secondary_background
-set --global fish_pager_color_secondary_completion
-set --global fish_pager_color_secondary_description
-set --global fish_pager_color_secondary_prefix
-set --global fish_pager_color_selected_background --background=FFCC66
-set --global fish_pager_color_selected_completion
-set --global fish_pager_color_selected_description
-set --global fish_pager_color_selected_prefix
+fish_config theme choose catppuccin-macchiato
 
 # keybinds
 set --global fish_key_bindings fish_default_key_bindings
