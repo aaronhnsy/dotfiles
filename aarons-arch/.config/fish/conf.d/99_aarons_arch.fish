06a19c94e9e41781253bf7c4f3476c8887f5e5ea
aaronhnsy <aaronhnsy@gmail.com>
2026-02-06 00:03:54 +0000

    add arch dotfiles
    Signed-off-by: aaronhnsy <aaronhnsy@gmail.com>

---
+++ aarons-arch/.config/fish/conf.d/99_aarons_arch.fish
@@ -0,0 +1,2 @@
+# set SSH_AUTH_SOCK to the 1Password agent socket
+set -gx SSH_AUTH_SOCK "$HOME/.1password/agent.sock"
--- aarons-pc-ubuntu/.config/fish/conf.d/99_aarons_pc_ubuntu.fish
+++
@@ -1,6 +0,0 @@
-# add windows ssh binaries to the path
-fish_add_path -g "/mnt/c/program files/openssh"
-
-# use the windows binaries for ssh and ssh-add
-alias ssh     "ssh.exe"
-alias ssh-add "ssh-add.exe"
